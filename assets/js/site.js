96f38a3fa59b41e52330152e26ece097b4f13538
PDionys <96830301+PDionys@users.noreply.github.com>
2025-01-12 17:28:22 +0200

    Add files via upload

---
+++ assets/js/site.js
@@ -0,0 +1,20 @@
+﻿// Please see documentation at https://learn.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+function contactMe(button) {
+    var mail = "p.deniskpi@gmail.com";
+
+    navigator.clipboard.writeText(mail).then(function () {
+        if (button !== null) {
+            alert("Mail copied to clipboard!");
+            button.innerText = mail;
+        } else {
+            alert("Mail: " + mail + " copied to clipboard!");
+        }
+        
+    }).catch(function (error) {
+        alert("Failed to copy text: " + error);
+    });
+}
